6ee6d32fc09ecf6b7867df24db38b42cbb824938
TerenceLyu <terencelyu96@gmail.com>
2019-10-09 23:23:39 -0700

    redo eval

--- src/Run.java
+++ src/Run.java
@@ -18,12 +18,14 @@ public class Run
 			new Point(3,0), new Point(4,0), new Point(0,1), new Point(1,1),
 			new Point(2,1), new Point(3,1), new Point(4,1), new Point(0,2),
 			new Point(1,2), new Point(2,2), new Point(3,2), new Point(0,3),
-			new Point(1,3), new Point(2,3), new Point(0,4), new Point(1,4)};
+			new Point(1,3), new Point(2,3), new Point(0,4), new Point(1,4)};//black bases
 	final static Point[] wBase={new Point(15,15), new Point(14,15), new Point(13,15),
 			new Point(12,15), new Point(11,15), new Point(15,14), new Point(14,14),
 			new Point(13,14), new Point(12,14), new Point(11,14), new Point(15,13),
 			new Point(14,13), new Point(13,13), new Point(12,13), new Point(15,12),
-			new Point(14,12), new Point(13,12), new Point(15,11), new Point(14,11)};
+			new Point(14,12), new Point(13,12), new Point(15,11), new Point(14,11)};//white bases
+	final static ArrayList<Point> bPieces = new ArrayList<>(19);//black pieces
+	final static ArrayList<Point> wPieces = new ArrayList<>(19);//white pieces
 	static long time = System.currentTimeMillis();
 	public static void main(String[] args) throws Exception
 	{
@@ -38,9 +40,17 @@ public class Run
 			charBoard[i] = input.readLine().toCharArray();
 		}
 		
-		HashMap<Point, Integer> board = buildBoard(charBoard);
-		ArrayList<Point> nextMove = treeSearch(board, player);
-		System.out.println(nextMove.stream().map(Objects::toString).collect(Collectors.joining()));
+		buildBoard(charBoard);
+//		System.out.println(bPieces);
+//		System.out.println(wPieces);
+		for (ArrayList<Point> list:generateMove(new Point(9,9)))
+		{
+			System.out.println(list);
+		}
+//		System.out.println(generateMove(new Point(15,15)).toString());
+//		System.out.println(eval(player));
+//		ArrayList<Point> nextMove = treeSearch(player);
+//		System.out.println(nextMove);
 		
 //		System.out.println(eval(board, 'B'));
 //		System.out.println(eval(board, 'W'));
@@ -56,124 +66,105 @@ public class Run
 //		}
 		writer.close();
 	}
-	private static ArrayList<Point> treeSearch(HashMap<Point, Integer> board, int player)
+	private static ArrayList<Point> treeSearch(int player)
 	{
-		int max = -1;
+		int max = -10000;
 		ArrayList<Point> nextMove = new ArrayList<>();
-		for (Map.Entry<Point, Integer> e:board.entrySet())
+		ArrayList<Point> pieces = (player==0) ? bPieces:wPieces;
+		for (Point p:pieces)
 		{
-			if (e.getValue() == player)
+			System.out.println(max);
+			System.out.println(p);
+			System.out.println(System.currentTimeMillis()-time+"ms");
+			ArrayList<ArrayList<Point>> moves = generateMove(p);
+			
+			for (ArrayList<Point> move:moves)
 			{
-				System.out.println(max);
-				System.out.println(System.currentTimeMillis()-time+"ms");
-				ArrayList<ArrayList<Point>> moves = generateMove(board, e.getKey(), player);
-				for (ArrayList<Point> move:moves)
+//				System.out.println(move.toString());
+				p.setLocation(move.get(move.size()-1));
+				int value = minimax(player, DEPTH, max, 10000, false);
+				if (value>max)
 				{
-					HashMap<Point, Integer> nBoard = (HashMap<Point, Integer>)board.clone();
-					nBoard.put(move.get(move.size()-1), board.get(move.get(0)));
-					nBoard.remove(move.get(0));
-
-					
-					int value = recSearch(nBoard, (player+1)%2, 0, max, 10000);
-					if (value>max)
-					{
-						max = value;
-						nextMove = move;
-					}
-					
+					max = value;
+					nextMove = move;
+					System.out.println(nextMove.toString());
 				}
+				p.setLocation(move.get(0));
 			}
 		}
 		return nextMove;
 	}
-	private static int recSearch(HashMap<Point, Integer> board, int player, int level, int alpha, int beta)
+	private static int minimax(int player, int level, int alpha, int beta, boolean maxOrMin)
 	{
-		int value = (level%2 == 0) ? -1:10000;
-		for (Map.Entry<Point, Integer> e:board.entrySet())
+		if (level == 0)
 		{
-			if (e.getValue() == player)
+//			System.out.println("leaf");
+			return eval(player);
+		}
+		ArrayList<Point> pieces;
+		ArrayList<ArrayList<Point>> moves;
+		if (maxOrMin)
+		{
+			int value = -1000000;
+			pieces = (player==0) ? bPieces:wPieces;
+			for (Point p:pieces)
 			{
-				ArrayList<ArrayList<Point>> moves = generateMove(board, e.getKey(), player);
+				moves = generateMove(p);
 				for (ArrayList<Point> move:moves)
 				{
-					HashMap<Point, Integer> nBoard = (HashMap<Point, Integer>)board.clone();
-//					board.remove(e.getKey());
-//					board.put(move.get(move.size()-1), player);
-					nBoard.put(move.get(move.size()-1), board.get(move.get(0)));
-					nBoard.remove(move.get(0));
-					//check if we are at leaf
-					if (level == DEPTH)
+					p.setLocation(move.get(move.size()-1));
+					value = Math.max(value, minimax(player, level-1, alpha, beta, false));
+					alpha = Math.max(alpha, value);
+					p.setLocation(move.get(0));
+					if (alpha>=beta)
 					{
-//						System.out.println("leaf");
-						if (level%2 == 0)
-						{
-							//max level
-							value = Math.max(value, eval(nBoard, player));
-						}else
-						{
-							//min level
-							value = Math.min(value, eval(nBoard, player));
-						}
-					}else
-					{
-						if (level%2 == 0)
-						{
-							//max level
-							value = Math.max(value, recSearch(nBoard, (player+1)%2, level+1, value, beta));
-						}else
-						{
-							//min level
-							value = Math.min(value, recSearch(nBoard, (player+1)%2, level+1, alpha, value));
-						}
+						return value;
 					}
-					//compare with either alpha or beta for pruning
-					if (level%2 == 0)
-					{
-						//at max
-						if (value>beta)
-						{
-							return value;
-						}
-					}else
-					{
-						//at min
-						if (value<alpha)
-						{
-							return value;
-						}
-					}
-//					board.remove(move.get(move.size()-1));
-//					board.put(e.getKey(), player);
-					
 				}
 			}
+			return value;
+		}else
+		{
+			int value = 1000000;
+			pieces = (player==0) ? wPieces:bPieces;
+			for (Point p:pieces)
+			{
+				moves = generateMove(p);
+				for (ArrayList<Point> move:moves)
+				{
+					p.setLocation(move.get(move.size()-1));
+					value = Math.min(value, minimax(player, level-1, alpha, beta, true));
+					beta = Math.min(beta, value);
+					p.setLocation(move.get(0));
+					if (alpha>=beta)
+					{
+						return value;
+					}
+				}
+			}
+			return value;
 		}
-		return value;
 	}
-	private static int eval(HashMap<Point, Integer> board, int player)
+	private static int eval(int player)
 	{
-		Point[] base = (player=='B') ? wBase:bBase;
+//		Point[] myBase = (player==0) ? bBase:wBase;
+//		Point[] oppoBase = (player==0) ? wBase:bBase;
 		int score = 0;
-		for (int i = 0; i < 19; i++)
+		int x = (player==0) ? 0:15;
+		int y = (player==0) ? 15:0;
+		ArrayList<Point> myPieces = (player==0) ? bPieces : wPieces;
+		ArrayList<Point> oppoPieces = (player==0) ? wPieces : bPieces;
+		for (Point p:myPieces)
 		{
-			//each occupied opponent's base grid is worth 10 points
-			if (board.containsKey(base[i]))
-			{
-				score += 10;
-			}
+			score += Math.abs(x-p.x) + Math.abs(x-p.y);
 		}
-		int x = (player==0) ? 15:0;
-		int y = x;
-		for (Map.Entry<Point, Integer> e:board.entrySet())
+		for (Point p:oppoPieces)
 		{
-			if (e.getValue().equals(player))
-			{
-				score += Math.min(30 - (Math.abs(x-e.getKey().x) + Math.abs(y-e.getKey().y)), 25);
-			}
+			score -= Math.abs(y-p.x) + Math.abs(y-p.y);
 		}
-		return score-50;
+		return score;
 	}
-	private static ArrayList<ArrayList<Point>> generateMove(HashMap<Point, Integer> board, Point piece, int player)
+	private static ArrayList<ArrayList<Point>> generateMove(Point piece)
 	{
 		ArrayList<ArrayList<Point>> listsOfMoves = new ArrayList<>();
 		for (int i = -1; i < 2; i++)
@@ -182,68 +173,111 @@ public class Run
 			{
 				if (i!=0||j!=0)
 				{
-					Point target = new Point(piece.x + i, piece.y + j);
-					if (!board.containsKey(target))
+					if (piece.x + i<SIZE&&piece.x + i>=0&&piece.y + j<SIZE&&piece.y + j>=0)
 					{
-						//regular moves
-						ArrayList<Point> moves = new ArrayList<>();
-						moves.add(piece);
-						moves.add(target);
-						listsOfMoves.add(moves);
-					}else
-					{
-						//check if a hop is possible
-						Point hop = new Point(piece.x + 2*i, piece.y + 2*j);
-						if (!board.containsKey(hop))
+						Point target = new Point(piece.x + i, piece.y + j);
+						if (!bPieces.contains(target)&&!wPieces.contains(target))
 						{
+							//regular moves
 							ArrayList<Point> moves = new ArrayList<>();
 							moves.add(piece);
-							moves.add(hop);
+							moves.add(target);
 							listsOfMoves.add(moves);
-							continousHop(listsOfMoves, moves, board);
+						}else
+						{
+							//check if a hop is possible
+							if (piece.x + 2*i<SIZE&&piece.x + 2*i>=0&&piece.y + 2*j<SIZE&&piece.y + 2*j>=0)
+							{
+								Point hop = new Point(piece.x + 2*i, piece.y + 2*j);
+								if (!bPieces.contains(hop)&&!wPieces.contains(hop))
+								{
+									ArrayList<Point> moves = new ArrayList<>();
+									moves.add(piece);
+									moves.add(hop);
+									listsOfMoves.add(moves);
+//									System.out.println(moves);
+									continousHop(listsOfMoves, moves);
+								}
+							}
 						}
 					}
+					
 				}
 			}
 		}
 		return listsOfMoves;
 	}
-	private static void continousHop(ArrayList<ArrayList<Point>> LOM, ArrayList<Point> LM, HashMap<Point, Integer> board)
+	private static void continousHop(ArrayList<ArrayList<Point>> LOM, ArrayList<Point> LM)
 	{
 		Point current = LM.get(LM.size()-1);
 		for (int i = -1; i < 2; i++)
 		{
 			for (int j = -1; j < 2; j++)
 			{
-				Point p = new Point(current.x-i, current.y-j);
-				if (board.containsKey(p)){
-					Point hop = new Point(current.x-2*i, current.y-2*j);
-					if (!board.containsKey(hop) && !LM.contains(hop))
-					{
-						ArrayList<Point> move = (ArrayList<Point>)LM.clone();
-						move.add(hop);
-						LOM.add(move);
-						continousHop(LOM, move, board);
+				if (current.x + 2*i<SIZE&&current.x + 2*i>=0&&current.y + 2*j<SIZE&&current.y + 2*j>=0)
+				{
+					Point p = new Point(current.x-i, current.y-j);
+					if (bPieces.contains(p)||wPieces.contains(p)){
+						Point hop = new Point(current.x-2*i, current.y-2*j);
+						if (!bPieces.contains(hop)&&!wPieces.contains(hop) && !LM.contains(hop))
+						{
+							ArrayList<Point> move = (ArrayList<Point>)LM.clone();
+							move.add(hop);
+//							System.out.println(move.toString());
+							LOM.add(move);
+							continousHop(LOM, move);
+						}
 					}
 				}
 			}
 		}
 	}
-	private static HashMap<Point, Integer> buildBoard(char[][] cBoard)
+	private static void buildBoard(char[][] cBoard)
 	{
-		HashMap<Point, Integer> board = new HashMap<>(40);
 		for (int i = 0; i < SIZE; i++)
 		{
 			for (int j = 0; j < SIZE; j++)
 			{
 				if (cBoard[i][j] != '.'){
-					board.put(new Point(j, i), (cBoard[i][j] == 'B') ? 0:1);
+					if (cBoard[i][j] == 'B')
+					{
+						bPieces.add(new Point(j, i));
+					}else
+					{
+						wPieces.add(new Point(j, i));
+					}
 				}
 			}
 		}
-		return board;
+	}
+}
+class Piece
+{
+	Point location;
+	
+	public Piece(Point location, int player)
+	{
+		this.location = location;
 	}
 	
+	@Override
+	public int hashCode()
+	{
+		return this.location.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		Piece other = (Piece) obj;
+		return this.location.equals(other.location);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return this.location.toString();
+	}
 }
 //class Board
 //{
