41637f56aa9430e0ae2cb52102fdf57a83cce7f3
TerenceLyu <34435235+TerenceLyu@users.noreply.github.com>
2019-10-11 01:48:33 -0700

    ai functioning

--- src/Run.java
+++ src/Run.java
@@ -13,7 +13,7 @@ import java.util.stream.Collectors;
 public class Run
 {
 	final static int SIZE = 16;
-	final static int DEPTH = 2;
+	static int DEPTH = 4;
 	final static Point[] bBase={new Point(0,0), new Point(1,0), new Point(2,0),
 			new Point(3,0), new Point(4,0), new Point(0,1), new Point(1,1),
 			new Point(2,1), new Point(3,1), new Point(4,1), new Point(0,2),
@@ -24,6 +24,8 @@ public class Run
 			new Point(13,14), new Point(12,14), new Point(11,14), new Point(15,13),
 			new Point(14,13), new Point(13,13), new Point(12,13), new Point(15,12),
 			new Point(14,12), new Point(13,12), new Point(15,11), new Point(14,11)};//white bases
+	final static Set<Point> bSet = new HashSet<>(Arrays.asList(bBase));
+	final static Set<Point> wSet = new HashSet<>(Arrays.asList(wBase));
 	final static ArrayList<Point> bPieces = new ArrayList<>(19);//black pieces
 	final static ArrayList<Point> wPieces = new ArrayList<>(19);//white pieces
 	static long time = System.currentTimeMillis();
@@ -34,6 +36,13 @@ public class Run
 		String mode = input.readLine();
 		int player = (input.readLine().equals("BLACK")) ? 0:1;
 		double timeLeft = Double.parseDouble(input.readLine());
+		if (mode.equals("SINGLE"))
+		{
+			DEPTH = (int) timeLeft/10 + 2;
+		}else
+		{
+			DEPTH = (int) timeLeft/100 + 2;
+		}
 		char[][] charBoard = new char[SIZE][SIZE];
 		for (int i = 0; i < SIZE; i++)
 		{
@@ -41,53 +50,72 @@ public class Run
 		}
 		
 		buildBoard(charBoard);
+//		System.out.println(checkWin(player-1));
 //		System.out.println(bPieces);
 //		System.out.println(wPieces);
-		for (ArrayList<Point> list:generateMove(new Point(9,9)))
-		{
-			System.out.println(list);
-		}
-//		System.out.println(generateMove(new Point(15,15)).toString());
+//		System.out.println(generateMove(new Point(1,0)).toString());
 //		System.out.println(eval(player));
-//		ArrayList<Point> nextMove = treeSearch(player);
-//		System.out.println(nextMove);
+		ArrayList<Point> nextMove = treeSearch(player);
+		System.out.println(nextMove);
 		
 //		System.out.println(eval(board, 'B'));
 //		System.out.println(eval(board, 'W'));
 
-//		String s = generateMove(board, new Point(2, 2), 1).stream().map(Object::toString)
-//				.collect(Collectors.joining(", "));
-//		System.out.println(s);
+		
+		if (nextMove.size()==2&&Math.abs(nextMove.get(0).x - nextMove.get(1).x) + Math.abs(nextMove.get(0).y - nextMove.get(1).y) <=2)
+		{
+			output("E", nextMove);
+		}else
+		{
+			output("J", nextMove);
+		}
+		System.out.println((System.currentTimeMillis()-time)/1000.0+"s");
+		
+	}
+	private static void output(String s, ArrayList<Point> nextMove) throws Exception
+	{
 		BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
 		
-//		for (Map.Entry<Point, Integer> e:board.entrySet())
-//		{
-//			writer.write(e.getKey().toString() + e.getValue() + "\n");
-//		}
+		for (int i = 0; i < nextMove.size()-1; i++)
+		{
+			writer.write(s);
+			writer.write(" "+nextMove.get(i).x+","+nextMove.get(i).y);
+			writer.write(" "+nextMove.get(i+1).x+","+nextMove.get(i+1).y + "\n");
+		}
+		
 		writer.close();
+		
 	}
 	private static ArrayList<Point> treeSearch(int player)
 	{
 		int max = -10000;
 		ArrayList<Point> nextMove = new ArrayList<>();
 		ArrayList<Point> pieces = (player==0) ? bPieces:wPieces;
+//		System.out.println(pieces);
 		for (Point p:pieces)
 		{
-			System.out.println(max);
-			System.out.println(p);
-			System.out.println(System.currentTimeMillis()-time+"ms");
+//			System.out.println(max);
+			System.out.println((System.currentTimeMillis()-time)/1000.0+"s");
 			ArrayList<ArrayList<Point>> moves = generateMove(p);
-			
+//			System.out.println(p);
+//			System.out.println(moves);
+//			System.out.println(pieces);
 			for (ArrayList<Point> move:moves)
 			{
 //				System.out.println(move.toString());
 				p.setLocation(move.get(move.size()-1));
-				int value = minimax(player, DEPTH, max, 10000, false);
+				
+				if (checkWin(player))
+				{
+					return move;
+				}
+				int value = minimax(player, DEPTH, max, 1000000, false);
 				if (value>max)
 				{
 					max = value;
 					nextMove = move;
-					System.out.println(nextMove.toString());
+//					System.out.println(max);
+//					System.out.println(nextMove.toString());
 				}
 				p.setLocation(move.get(0));
 			}
@@ -113,6 +141,16 @@ public class Run
 				for (ArrayList<Point> move:moves)
 				{
 					p.setLocation(move.get(move.size()-1));
+					if (checkWin(player))
+					{
+						p.setLocation(move.get(0));
+						return 9999-level;
+					}
+					if (checkWin((player+1)%2))
+					{
+						p.setLocation(move.get(0));
+						return -9999+level;
+					}
 					value = Math.max(value, minimax(player, level-1, alpha, beta, false));
 					alpha = Math.max(alpha, value);
 					p.setLocation(move.get(0));
@@ -145,22 +183,77 @@ public class Run
 			return value;
 		}
 	}
+	private static boolean checkWin(int player)
+	{
+		Set<Point> base = (player == 0) ? wSet:bSet;
+		ArrayList<Point> myPieces = (player==0) ? bPieces : wPieces;
+		ArrayList<Point> oppoPieces = (player==0) ? wPieces : bPieces;
+		int count = 0;
+		for (Point p:myPieces)
+		{
+			if (base.contains(p))
+			{
+				count++;
+			}
+		}
+		if (count==0){
+			return false;
+		}
+		for (Point p:oppoPieces)
+		{
+			if (base.contains(p))
+			{
+				count++;
+			}
+		}
+		return count==19;
+	}
 	private static int eval(int player)
 	{
-//		Point[] myBase = (player==0) ? bBase:wBase;
-//		Point[] oppoBase = (player==0) ? wBase:bBase;
+		Set<Point> myBase = (player==0) ? bSet:wSet;
+		Set<Point> oppoBase = (player==0) ? wSet:bSet;
 		int score = 0;
 		int x = (player==0) ? 0:15;
 		int y = (player==0) ? 15:0;
 		ArrayList<Point> myPieces = (player==0) ? bPieces : wPieces;
 		ArrayList<Point> oppoPieces = (player==0) ? wPieces : bPieces;
+		int iWin = 0;
+		int iLose = 0;
 		for (Point p:myPieces)
 		{
 			score += Math.abs(x-p.x) + Math.abs(x-p.y);
+			if (myBase.contains(p))
+			{
+				score -=10;
+				iLose++;
+			}
+			if (oppoBase.contains(p))
+			{
+				score +=10;
+				iWin++;
+			}
 		}
 		for (Point p:oppoPieces)
 		{
 			score -= Math.abs(y-p.x) + Math.abs(y-p.y);
+			if (myBase.contains(p))
+			{
+				score -=10;
+				iLose++;
+			}
+			if (oppoBase.contains(p))
+			{
+				score +=10;
+				iWin++;
+			}
+		}
+		if (iWin == 19)
+		{
+			return 9999;
+		}
+		if (iLose == 19)
+		{
+			return -9999;
 		}
 		return score;
 	}
@@ -180,7 +273,7 @@ public class Run
 						{
 							//regular moves
 							ArrayList<Point> moves = new ArrayList<>();
-							moves.add(piece);
+							moves.add(piece.getLocation());
 							moves.add(target);
 							listsOfMoves.add(moves);
 						}else
@@ -192,7 +285,7 @@ public class Run
 								if (!bPieces.contains(hop)&&!wPieces.contains(hop))
 								{
 									ArrayList<Point> moves = new ArrayList<>();
-									moves.add(piece);
+									moves.add(piece.getLocation());
 									moves.add(hop);
 									listsOfMoves.add(moves);
 //									System.out.println(moves);
@@ -251,34 +344,34 @@ public class Run
 		}
 	}
 }
-class Piece
-{
-	Point location;
-	
-	public Piece(Point location, int player)
-	{
-		this.location = location;
-	}
-	
-	@Override
-	public int hashCode()
-	{
-		return this.location.hashCode();
-	}
-	
-	@Override
-	public boolean equals(Object obj)
-	{
-		Piece other = (Piece) obj;
-		return this.location.equals(other.location);
-	}
-	
-	@Override
-	public String toString()
-	{
-		return this.location.toString();
-	}
-}
+//class Piece
+//{
+//	Point location;
+//
+//	public Piece(Point location, int player)
+//	{
+//		this.location = location;
+//	}
+//
+//	@Override
+//	public int hashCode()
+//	{
+//		return this.location.hashCode();
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj)
+//	{
+//		Piece other = (Piece) obj;
+//		return this.location.equals(other.location);
+//	}
+//
+//	@Override
+//	public String toString()
+//	{
+//		return this.location.toString();
+//	}
+//}
 //class Board
 //{
 //}
